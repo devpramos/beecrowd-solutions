d253e3bb413bb9b48654f04f278baaa5b4673a20
Pedro Henrique De Oliveira Ramos <pramospedro@icloud.com>
2025-10-18 16:20:49 -0300

    feat: Adiciona solução para o problema 1037 em C#

---
+++ Iniciante/📝 1037 - Intervalo/solucao.cs
@@ -0,0 +1,23 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    double n = double.Parse(Console.ReadLine());
+    
+    if (n >= 0 && n <= 25) {
+        Console.WriteLine("Intervalo [0,25]");
+    } else if (n > 25 && n <= 50) {
+        Console.WriteLine("Intervalo (25,50]");
+    } else if (n > 50 && n <= 75) {
+        Console.WriteLine("Intervalo (50,75]");
+    } else if (n > 75 && n <= 100) {
+        Console.WriteLine("Intervalo (75,100]");
+    } else {
+        Console.WriteLine("Fora de intervalo");
+    }
+    
+    }
+
+}
