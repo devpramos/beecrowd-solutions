f23829e6b29580b684370a93003bb62244297d5f
Pedro Henrique De Oliveira Ramos <pramospedro@icloud.com>
2025-10-24 09:27:15 -0300

    feat: Adiciona solução para o problema 1078 em C#

---
+++ Iniciante/📝 1078 - Tabuada/solucao.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        if (2 < n && n < 1000)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {n} = {i * n}");
+            }
+        }
+    }
+
+}
